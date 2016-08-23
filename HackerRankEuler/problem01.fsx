19ee6653691e9ffb44a1ab692ecd5018869b3e2f
Andre Dublin <81dublin@gmail.com>
2016-08-23 12:52:06 -0400

    update
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- HackerRankEuler/problem01.fsx
+++ HackerRankEuler/problem01.fsx
@@ -1,6 +1,6 @@
 ﻿open System
 
-let sumOfMultiples (offset : bigint, limit : bigint) : bigint = 
+let sumOfMultiples (offset : bigint, limit : bigint) : bigint =
     let length = (limit - 1I) / offset
     let last = offset + (length - 1I) * offset
     length * (offset + last) / 2I
@@ -9,6 +9,6 @@ let parse = Console.ReadLine
 
 [ 1..(int (parse())) ]
 |> List.map (fun _ -> int (parse()))
-|> List.map 
+|> List.map
        (fun x -> sumOfMultiples (3I, bigint (x)) + sumOfMultiples (5I, bigint (x)) - sumOfMultiples (15I, bigint (x)))
 |> List.iter Console.WriteLine
--- HackerRankEuler/problem03.fsx
+++ HackerRankEuler/problem03.fsx
@@ -1,7 +1,7 @@
 ﻿open System
 
 // a prime number is a whole number greater than 1, whose only two whole-number factors are 1 and itself
-let isPrime (n : int64) = 
+let isPrime (n : int64) =
     // For a number like 17 it would take 7 iterations until the left hand side evaluates true
     // imagine the number of iterations for int64 max or 9223372036854775807
     // unfortunately this check function time space becomes O(n^2) or O(n!) for larger ints
@@ -12,7 +12,7 @@ let parse = Console.ReadLine
 let cases = [ 1..(int (parse())) ] |> List.map (fun _ -> int64 (parse()))
 
 for x in cases do
-    seq { 
+    seq {
         for i in 2L..x do
             if x % i = 0L && isPrime i then yield i
     }
